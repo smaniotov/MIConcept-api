4176a725c402845d669b1eaf835b69986810831c
smaniotov <smaniotov@gmail.com>
2018-06-13 01:20:33 -0300

    add index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-/**
- * Index file that join all api code
- */
+const express = require("express")
+const app = express()
+const {todo} = require("./api")
+const hostname = 'local.miconcept.com'
+const port = '9000'
+
+console.log(`Server running on port: ${port}`)
+
+app.use('/todo', todo)
+
+app.listen(port, hostname)
